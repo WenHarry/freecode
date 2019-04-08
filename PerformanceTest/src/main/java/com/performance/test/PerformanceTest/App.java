15c56bb1cec96a2bb2f4cea3db554a063e97a28f
WenHarry <417324714@qq.com>
2019-04-08 15:09:13 +0800

    性能自动化代码提交

---
+++ PerformanceTest/src/main/java/com/performance/test/PerformanceTest/App.java
@@ -0,0 +1,13 @@
+package com.performance.test.PerformanceTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
